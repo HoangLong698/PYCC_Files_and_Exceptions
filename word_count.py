4edabb8023a1348f4eaa1a80a31f13f44fbb342e
HoangLong698 <tranhuuhoanglong248@gmail.com>
2022-01-08 13:27:12 +0700

    Count word multi files

--- word_count.py
+++ word_count.py
@@ -4,11 +4,16 @@ def count_words(filename):
         with open(filename, encoding='utf-8') as f:
             contents = f.read()
     except FileNotFoundError:
-        print(f"Sorry, the file {filename} does not exist.")
+        # print(f"Sorry, the file {filename} does not exist.")
+        pass
     else:
         # count the approximate number of words in the file.
         words = contents.split()
         num_words = len(words)
         print(f"the file {filename} has about {num_words} words.")
-filename = 'alice.txt'
-count_words(filename)
+# filename = 'alice.txt'
+# count_words(filename)
+
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    count_words(filename)
